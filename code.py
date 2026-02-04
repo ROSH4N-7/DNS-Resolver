a2fe84022a4150c24f7b97b8dd04b7f69d122d35
ROSH4N-7 <khanalroshan305@gmail.com>
2026-02-04 10:19:06 +0545

    Added function to read SSL certificate details

--- code.py
+++ code.py
@@ -5,3 +5,14 @@ import tkinter as tk
 from tkinter import messagebox
 #Storing resolved domain records
 records= []
+
+# Collected SSL certificate issued and expiry dates
+def get_ssl_info(domain):
+    try:
+        context = ssl.create_default_context()
+        with socket.create_connection((domain, 443), timeout=5) as sock:
+            with context.wrap_socket(sock, server_hostname=domain) as ssock:
+                cert = ssock.getpeercert()
+                return cert.get('notBefore', 'N/A'), cert.get('notAfter', 'N/A')
+    except:
+        return "N/A", "N/A"
